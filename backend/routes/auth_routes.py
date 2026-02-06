ebd4b7e5d44281acc310ec641b5225b96e844192
danny <vilde-26@rhodes.edu>
2026-02-06 10:26:13 -0600

    dummy page

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
-router = APIRouter()
+router = APIRouter(prefix='/auth', tags=['auth'])
 
+class LoginRequest(BaseModel):
+    email: str
 
 @router.post("/login")
-def login():
-    return {"message": "Login endpoint working"}
+def login(data: LoginRequest):
+    if data.email.endswith("@admin.edu"):
+        role: "admin"
+    else:
+        role = "user"
+
+    return {"message": "Login endpoint working",
+            "user": {
+                "email": data.email,
+                "role": role
+            }
+        }
