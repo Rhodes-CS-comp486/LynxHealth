663d3d72be76b2aea83d8346fe3118026332a285
MacKenzie Roach <roamf-26@rhodes.edu>
2026-03-26 19:43:28 -0500

    Fix resources page API path for production

--- frontend/src/app/resources/resources.component.ts
+++ frontend/src/app/resources/resources.component.ts
@@ -33,7 +33,7 @@ export class ResourcesComponent implements OnInit {
   addedSections: PageSection[] = [];
   sectionMap: { [key: string]: PageSection } = {};
 
-  private readonly apiUrl = 'http://localhost:8000/pages';
+  private readonly apiUrl = '/api/pages';
 
   constructor(private readonly cdr: ChangeDetectorRef) {}
 
